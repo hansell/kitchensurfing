5d0162d942fcee9b20c2e921785880190a0321c2
hansell <xiaoyingfeng2012@hotmail.com>
2014-05-23 15:59:27 +0800

    md5 encode

--- src/main/java/com/kitchensurfing/common/MD5MessageDigest.java
+++ src/main/java/com/kitchensurfing/common/MD5MessageDigest.java
@@ -15,10 +15,9 @@ public class MD5MessageDigest implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	@SuppressWarnings("unused")
 	public static String Md5Encode(String args) throws NoSuchAlgorithmException{
 		MessageDigest messageDigest = MessageDigest.getInstance("MD5");  
-		messageDigest.update("666666".getBytes(),0, "666666".length());  
+		messageDigest.update(args.getBytes(),0, args.length());  
 		String hashedPass = new BigInteger(1,messageDigest.digest()).toString(16);  
 		if (hashedPass.length() < 32) {
 		   hashedPass = "0" + hashedPass; 
