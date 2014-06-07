2e57951af7e6e4ef91992e5833f1facea00bc6e5
hansell <xiaoyingfeng2012@hotmail.com>
2014-06-07 22:17:00 +0800

    exception

---
+++ src/main/java/com/kitchensurfing/exception/HandleException.java
@@ -0,0 +1,8 @@
+package com.kitchensurfing.exception;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.io.Serializable;
+
+//@ExceptionHandler(IllegalArgumentException.class)
+public class HandleException implements Serializable {
+
+}
